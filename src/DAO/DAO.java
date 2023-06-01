56b2cef594888e1af089ed3a2114a5245299cc38
Nguyen Thi Lan Anh <nguyenlananh25112003@gmail.com>
2023-06-01 13:45:00 +0700

    them

--- src/DAO/DAO.java
+++ src/DAO/DAO.java
@@ -4,18 +4,19 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
  
-public class ConnectToXAMPP {
+public class DAO {
+    protected Connection con;
 
   public static void main(String[] args) throws SQLException {
     String url = "jdbc:mysql://localhost:3306/doangame";
     String user = "root";
     String password = "";
   
-    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+    try (Connection con = DriverManager.getConnection(url, user, password)) {
       System.out.println("Ket noi thanh cong");
-      System.out.println(conn.getCatalog());
+      System.out.println(con.getCatalog());
     } catch(SQLException ex) {
-      Logger.getLogger(ConnectToXAMPP.class.getName()).log(Level.SEVERE, "loi khi ket noi MySQL", ex);
+      Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, "loi khi ket noi MySQL", ex);
     }
   }
 }
